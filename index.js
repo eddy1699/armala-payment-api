4286b58b25ca137644a470ad472a3915642b178d
Edgardo <edgardosamame@gmail.com>
2024-09-16 16:44:04 -0500

    frst22;

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express()
 const port = 3000
 
 app.use(cors())
-app.options('*', cors()) 
+app.all('*', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  //...
+ });
 // Body parser
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
